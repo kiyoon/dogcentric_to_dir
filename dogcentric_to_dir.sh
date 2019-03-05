5c7d7ab5f3bbe1e69776b6e2be9c08c5b6f2a731
Kiyoon Kim <yoonkr33@gmail.com>
2019-03-04 21:03:02 -0500

    initial commit

---
+++ dogcentric_to_dir.sh
@@ -0,0 +1,20 @@
+
+if [ $# -lt 1 ]
+then
+	echo "Usage: $0 [dogcentric_dir]"
+	exit 1
+fi
+
+dataset_dir=$1
+find "$dataset_dir" -name "*.tar.gz" | xargs -i tar xzf {}
+rm -rf "$dataset_dir/Hime/"
+rm -rf "$dataset_dir/Ringo/"
+rm -rf "$dataset_dir/Ryu/"
+rm -rf "$dataset_dir/Ku/"
+
+# fix glitches
+rm -rf "$dataset_dir/Turn_Right/"		# Same as Look_at_Right but less samples.
+find "$dataset_dir" -name "Thumbs.db" | xargs -i rm {}
+find "$dataset_dir" -name "info.txt" | xargs -i rm {}
+rm -rf "$dataset_dir/Shake/Shake_Ku_1_1300_1390/"
+mv "$dataset_dir/Shake/Shake__Ku_8_1100_1230.avi" "$dataset_dir/Shake/Shake_Ku_8_1100_1230.avi"
